bce22dcee7625b8beb9670127555d12f90c00660
Ana <anamestree@gmail.com>
2020-01-31 15:32:10 +0100

    Q7

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -462,9 +462,19 @@ def foodHeuristic(state, problem):
     Subsequent calls to this heuristic can access
     problem.heuristicInfo['wallCount']
     """
+
     position, foodGrid = state
-    "*** YOUR CODE HERE ***"
-    return 0
+
+    """if problem.isGoalState(state):
+        return 0
+    return max([distance(corner, position) for corner in foodGrid.asList()])"""
+
+    # res = [0]
+    # for foodCoord in foodGrid.asList():
+    #     dist = mazeDistance(position, foodCoord, problem.startingGameState)
+    #     #print(dist)
+    #     res.append(dist)
+    return max([0]+[mazeDistance(position, foodCoord, problem.startingGameState) for foodCoord in foodGrid.asList()])
 
 class ClosestDotSearchAgent(SearchAgent):
     "Search for all food using a sequence of searches"
